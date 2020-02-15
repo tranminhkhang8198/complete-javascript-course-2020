c8f82ddaba6db729702dcb4d9cc96bebd634a4b2
tranminhkhang8198 <minhkhang811998@gmail.com>
2020-02-15 17:01:24 +0700

    Initial for budgety

---
+++ advanced-js/script.js
@@ -0,0 +1,119 @@
+// Function constructor
+
+/*
+var Person = function(name, yearOfBirth, job) {
+    this.name = name;
+    this.yearOfBirth = yearOfBirth;
+    this.job = job;
+}
+
+Person.prototype.calculateAge = function() {
+    console.log(2020 - this.yearOfBirth);
+}
+
+Person.prototype.lastName = 'Smith';
+
+var john = new Person('John', 1990, 'teacher');
+var jane = new Person('Jane', 1990, 'designer');
+
+john.calculateAge();
+jane.calculateAge();
+
+console.log(john.lastName);
+console.log(jane.lastName);
+*/
+
+
+// Object.create
+/*
+var personProto = {
+    calculateAge: function() {
+        console.log(2020 - this.yearOfBirth);
+    }
+};
+
+var john = Object.create(personProto);
+john.name = 'John';
+john.yearOfBirth = 1990;
+john.job = 'teacher';
+
+var jane = Object.create(personProto, {
+    name: { value: 'Jane' },
+    yearOfBirth: { value: 1969 },
+    job: { value: 'designer' }
+});
+*/
+
+// Primitives vs objects
+
+// Primitives
+var a = 23;
+var b = a;
+a = 46;
+console.log(a);
+console.log(b);
+
+
+// Objects
+/*
+var obj1 = {
+    name: 'John',
+    age: 26
+};
+
+var obj2 = obj1;
+obj1.age = 30;
+console.log(obj1.age);
+console.log(obj2.age);
+
+// Functions
+var age = 27;
+var obj = {
+    name: 'Jonas',
+    city: 'Lison'
+};
+
+function change(a, b) {
+    a = 30;
+    b.city = 'San Francisco';
+}
+
+change(age, obj);
+
+console.log(age);
+console.log(obj.city);
+*/
+
+
+// First Class Functions Passing Functions as Arguments
+var years = [1990, 1965, 1937, 2005, 1998];
+
+function arrayCalc(arr, fn) {
+    var arrRes = [];
+    for (var i = 0; i < arr.length; i++) {
+        arrRes.push(fn(arr[i]));
+    }
+}
+
+function calculateAge(el) {
+    return 2020 - el;
+}
+
+function isFullAge(el) {
+    return el >= 18;
+}
+
+var ages = arrayCalc(years, calculateAge);
+var fullAges = arrayCalc(ages, isFullAge);
+
+console.log(ages);
+console.log(fullAges);
+
+
+// Closure
+
+
+
+
+
+
