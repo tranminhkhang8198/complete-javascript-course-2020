e3a074fb88d4a95ca46a27ec745bd433b01352ef
tranminhkhang8198 <minhkhang811998@gmail.com>
2020-02-18 09:33:06 +0700

    Building the Search Model

--- forkify/src/js/index.js
+++ forkify/src/js/index.js
@@ -1,13 +1,7 @@
-import axios from 'axios';
+import Search from './models/Search';
 
-async function getResults(query) {
-    try {
-        const res = await axios(`https://forkify-api.herokuapp.com/api/search?&q=${query}`);
-        const recipes = res.data.recipes;
-        console.log(recipes);
-    } catch (err) {
-        alert(err);
-    }
-}
+const search = new Search('pizza');
 
-getResults('pizza');
+console.log(search);
+
+search.getResults();
