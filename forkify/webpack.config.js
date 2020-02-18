c3b7531f2f597005e731d95a30955f48fd7cba0b
tranminhkhang8198 <minhkhang811998@gmail.com>
2020-02-18 08:50:13 +0700

    A Modern Setup Babel

--- forkify/webpack.config.js
+++ forkify/webpack.config.js
@@ -2,7 +2,7 @@ const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-    entry: './src/js/index',
+    entry: ['babel-polyfill', './src/js/index'],
     output: {
         path: path.resolve(__dirname, 'dist'),
         filename: 'js/bundle.js'
@@ -15,5 +15,16 @@ module.exports = {
             filename: 'index.html',
             template: './src/index.html'
         })
-    ]
+    ],
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: 'babel-loader'
+                }
+            }
+        ]
+    }
 }
